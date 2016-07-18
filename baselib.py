5aeb5b1e1e4ee6f081e6b3f601252c2a78c95f3a
Andrea Zanoni <andrea.zanoni@polimi.it>
2016-07-18 15:19:27 +0200

    Fixed bug in importing .log file with upper and lower limits for nodes

--- baselib.py
+++ baselib.py
@@ -125,6 +125,7 @@ def parse_log_file(context):
         else:
             mbs.num_timesteps = mbs.num_rows/nn 
         mbs.is_ready = True
+        ret_val = {'FINISHED'}
     else:
         ret_val = {'NODES_NOT_FOUND'}
     pass 
