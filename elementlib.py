794750f7d472ed3b3747db7e44231a3c74af79bd
Janga Reddy <janga1997@localhost.localdomain>
2017-06-12 21:33:15 +0530

    Add revolute rotation joint

--- elementlib.py
+++ elementlib.py
@@ -65,6 +65,7 @@ def parse_elements(context, jnt_type, rw):
             "deformabledisplacementjoint": parse_deformable_displacement,
             "revolutehinge": parse_revolute_hinge,
             "revolutepin": parse_revolute_pin,
+            "revoluterotation": parse_revolute_rot,
             "rod": parse_rod,
             "rod bezier": parse_rod_bezier,
             "shell4" : parse_shell4,
--- revjlib.py
+++ revjlib.py
@@ -146,6 +146,71 @@ def parse_revolute_pin(rw, ed):
 # -------------------------------------------------------------------------- 
 # end of parse_revolute_pin(rw, ed) function
 
+# helper function to parse revolute rot joints
+def parse_revolute_rot(rw, ed):
+    ret_val = True
+    # Debug message
+    print("parse_revolute_rot(): Parsing revolute rot joint " + rw[1])
+    try:
+        el = ed['revolute_rot_' + str(rw[1])]
+
+        print("parse_revolute_rot(): found existing entry in elements dictionary. Updating it.")
+
+        el.nodes[0].int_label = int(rw[2])
+        el.nodes[1].int_label = int(rw[15])
+
+        el.offsets[0].value = Vector(( float(rw[3]), float(rw[4]), float(rw[5]) ))
+
+        R1 = Matrix()
+        parse_rotmat(rw, 6, R1)
+        el.rotoffsets[0].value = R1.to_quaternion();
+
+        el.offsets[1].value = Vector(( float(rw[16]), float(rw[17]), float(rw[18]) ))
+
+        R2 = Matrix()
+        parse_rotmat(rw, 19, R2)
+        el.rotoffsets[1].value = R2.to_quaternion();
+
+        if el.name in bpy.data.objects.keys():
+            el.blender_object = el.name
+        el.is_imported = True
+        pass
+    except KeyError:
+        print("parse_revolute_rot(): didn't find an entry in elements dictionary. Creating one.")
+        el = ed.add()
+        el.type = 'revolute_rot'
+        el.int_label = int(rw[1])
+
+        el.nodes.add()
+        el.nodes[0].int_label = int(rw[2])
+        el.nodes.add()
+        el.nodes[1].int_label = int(rw[15])
+
+        el.offsets.add()
+        el.offsets[0].value = Vector(( float(rw[3]), float(rw[4]), float(rw[5]) ))
+
+        el.rotoffsets.add()
+        R1 = Matrix()
+        parse_rotmat(rw, 6, R1)
+        el.rotoffsets[0].value = R1.to_quaternion();
+
+        el.offsets.add()
+        el.offsets[1].value = Vector(( float(rw[16]), float(rw[17]), float(rw[18]) ))
+
+        el.rotoffsets.add()
+        R2 = Matrix()
+        parse_rotmat(rw, 19, R2)
+        el.rotoffsets[1].value = R2.to_quaternion();
+
+        el.import_function = "add.mbdyn_elem_revolute_rot"
+        el.name = el.type + "_" + str(el.int_label)
+        el.is_imported = True
+        ret_val = False
+        pass
+    return ret_val
+# --------------------------------------------------------------------------
+# end of parse_revolue_rot(rw, ed) function
+
 # Creates the object representing a revolute hinge joint element
 def spawn_revolute_hinge_element(elem, context):
     """ Draws a revolute hinge joint element, loading a wireframe
@@ -182,7 +247,7 @@ def spawn_revolute_hinge_element(elem, context):
 
     # load the wireframe revolute joint object from the library
     app_retval = bpy.ops.wm.append(directory = os.path.join(mbs.addon_path,\
-            'library', 'joints.blend', 'Object'), filename = 'revolute')
+            'library', 'joints.blend', 'Object'), filename = 'revolute rotation')
     if app_retval == {'FINISHED'}:
         # the append operator leaves just the imported object selected
         revjOBJ = bpy.context.selected_objects[0]
@@ -411,3 +476,148 @@ class Scene_OT_MBDyn_Import_Revolute_Pin_Joint_Element(bpy.types.Operator):
 # -----------------------------------------------------------
 # end of Scene_OT_MBDyn_Import_Revolute_Pin_Joint_Element class
 
+# Creates the object representing a revolute rot joint element
+def spawn_revolute_rot_element(elem, context):
+    """ Draws a revolute rot joint element, loading a wireframe
+        object from the addon library """
+    mbs = context.scene.mbdyn
+    nd = mbs.nodes
+
+    if any(obj == elem.blender_object for obj in context.scene.objects.keys()):
+        return {'OBJECT_EXISTS'}
+        print("spawn_revolute_rot_element(): Element is already imported. \
+                Remove the Blender object or rename it \
+                before re-importing the element.")
+        return {'CANCELLED'}
+
+    try:
+        n1 = nd['node_' + str(elem.nodes[0].int_label)].blender_object
+    except KeyError:
+        print("spawn_revolute_rot_element(): Could not find a Blender \
+                object associated to Node " + \
+                str(elem.nodes[0].int_label))
+        return {'NODE1_NOTFOUND'}
+
+    try:
+        n2 = nd['node_' + str(elem.nodes[1].int_label)].blender_object
+    except KeyError:
+        print("spawn_revolute_rot_element(): Could not find a Blender \
+                object associated to Node " + \
+                str(elem.nodes[1].int_label))
+        return {'NODE2_NOTFOUND'}
+
+    # nodes' objects
+    n1OBJ = bpy.data.objects[n1]
+    n2OBJ = bpy.data.objects[n2]
+
+    # load the wireframe revolute joint object from the library
+    app_retval = bpy.ops.wm.append(directory = os.path.join(mbs.addon_path,\
+            'library', 'joints.blend', 'Object'), filename = 'revolute rotation')
+    if app_retval == {'FINISHED'}:
+        # the append operator leaves just the imported object selected
+        revjOBJ = bpy.context.selected_objects[0]
+        revjOBJ.name = elem.name
+
+        # automatic scaling
+        s = (.5/sqrt(3.))*(n1OBJ.scale.magnitude + \
+        n2OBJ.scale.magnitude)*elem.scale_factor
+        revjOBJ.scale = Vector(( s, s, s ))
+
+        # joint offsets with respect to nodes
+        f1 = elem.offsets[0].value
+        f2 = elem.offsets[1].value
+        q1 = elem.rotoffsets[0].value
+        q2 = elem.rotoffsets[1].value
+
+        # project offsets in global frame
+        R1 = n1OBJ.rotation_quaternion.to_matrix()
+        R2 = n2OBJ.rotation_quaternion.to_matrix()
+        p1 = n1OBJ.location + R1*Vector(( f1[0], f1[1], f1[2] ))
+        p2 = n2OBJ.location + R2*Vector(( f2[0], f2[1], f2[2] ))
+
+        # place the joint object in the position defined relative to node 1
+        revjOBJ.location = p1
+        revjOBJ.rotation_mode = 'QUATERNION'
+        revjOBJ.rotation_quaternion = \
+                n1OBJ.rotation_quaternion * Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+
+        # create an object representing the second RF used by the joint
+        # for model debugging
+        bpy.ops.object.empty_add(type = 'ARROWS', location = p2)
+        RF2 = bpy.context.selected_objects[0]
+        RF2.rotation_mode = 'QUATERNION'
+        RF2.rotation_quaternion = \
+                n2OBJ.rotation_quaternion * Quaternion(( q2[0], q2[1], q2[2], q2[3] ))
+        RF2.scale = .33*revjOBJ.scale
+        RF2.name = revjOBJ.name + '_RF2'
+        RF2.select = True
+        bpy.context.scene.objects.active = revjOBJ
+        bpy.ops.object.parent_set(type = 'OBJECT', keep_transform = False)
+        RF2.hide = True
+
+        # set parenting of wireframe obj
+        bpy.ops.object.select_all(action = 'DESELECT')
+        revjOBJ.select = True
+        n1OBJ.select = True
+        bpy.context.scene.objects.active = n1OBJ
+        bpy.ops.object.parent_set(type = 'OBJECT', keep_transform = False)
+
+        elem.blender_object = revjOBJ.name
+
+        return {'FINISHED'}
+    else:
+        return {'LIBRARY_ERROR'}
+# -----------------------------------------------------------
+# end of spawn_revolute_rot_element(elem, context) function
+
+# Imports a Revolute Joint in the scene
+class Scene_OT_MBDyn_Import_Revolute_Rot_Element(bpy.types.Operator):
+    bl_idname = "add.mbdyn_elem_revolute_rot"
+    bl_label = "MBDyn revolute joint element importer"
+    int_label = bpy.props.IntProperty()
+
+    def draw(self, context):
+        layout = self.layout
+        layout.alignment = 'LEFT'
+
+    def execute(self, context):
+        ed = bpy.context.scene.mbdyn.elems
+        nd = bpy.context.scene.mbdyn.nodes
+
+        try:
+            elem = ed['revolute_rot_' + str(self.int_label)]
+            retval = spawn_revolute_rot_element(elem, context)
+            if retval == {'OBJECT_EXISTS'}:
+                message = "Found the Object " + elem.blender_object + \
+                    " remove or rename it to re-import the element!"
+                self.report({'WARNING'}, message)
+                logging.warning(message)
+                return {'CANCELLED'}
+            elif retval == {'NODE1_NOTFOUND'}:
+                message = "Could not import element: Blender object " +\
+                    "associated to Node " + str(elem.nodes[0].int_label) \
+                    + " not found"
+                self.report({'ERROR'}, message)
+                logging.error(message)
+                return {'CANCELLED'}
+            elif retval == {'NODE2_NOTFOUND'}:
+                message = "Could not import element: Blender object " +\
+                        "associated to Node " + str(elem.nodes[1].int_label) + " not found"
+                self.report({'ERROR'}, message)
+                logging.error(message)
+                return {'CANCELLED'}
+            elif retval == {'LIBRARY_ERROR'}:
+                message = "Could not import element: could not " +\
+                        "load library object"
+                self.report({'ERROR'}, message)
+                logging.error(message)
+                return {'CANCELLED'}
+            else:
+                return retval
+        except KeyError:
+            message = "Element revolute_rot_" + str(elem.int_label) + "not found"
+            self.report({'ERROR'}, message)
+            logging.error(message)
+            return {'CANCELLED'}
+# -----------------------------------------------------------
+# end of Scene_OT_MBDyn_Import_Revolute_Joint_Element class
