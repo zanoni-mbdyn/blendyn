1a7fe932f8ea271b9d2b645df5af1f68e606ad08
Andrea Zanoni <andrea.zanoni@polimi.it>
2019-08-13 18:58:31 +0200

    fixed typos

--- defdispjlib.py
+++ defdispjlib.py
@@ -119,7 +119,7 @@ class BLENDYN_OT_import_deformable_displacement(bpy.types.Operator):
         nd = bpy.context.scene.mbdyn.nodes
     
         try:
-            elem = ed['defdispj_' + str(self.int_label)]
+            elem = ed['deformable_displacement_' + str(self.int_label)]
             return spawn_defdispj_element(elem, context)
         except KeyError:
             eldbmsg({'DICT_ERROR'}, type(self).__name__ + '::execute()', elem)
