dec792055f5ba2ba77a53fb615065fa94293f61a
Andrea Zanoni <andrea.zanoni@polimi.it>
2019-08-27 12:40:30 +0200

    Added deformable displacement joint

--- defdispjlib.py
+++ defdispjlib.py
@@ -23,6 +23,7 @@
 # -------------------------------------------------------------------------- 
 
 import bpy
+import os
 
 from mathutils import *
 from math import *
@@ -98,13 +99,84 @@ def parse_deformable_displacement(rw, ed):
 # end of parse_deformable_displacement(rw, ed) function
 
 # Creates the object representing a deformable displacement joint element
-def spawn_deformable_displacement_element(elem, context):
-    # TODO
-    return {'FINISHED'}
-# -----------------------------------------------------------
-# end of spawn_deformable_displacament_element(elem, context) function
+def spawn_deformable_displacement_joint_element(elem, context):
+    """ Draws a deformable displacement joint, loading a wireframe
+        object from the addon library """
+    mbs = context.scene.mbdyn
+    nd = mbs.nodes
+    
+    if any(obj == elem.blender_object for obj in context.scene.objects.keys()):
+        return {'OBJECT_EXISTS'}
 
-## Imports a Deformable Displacement Joint in the scene -- TODO
+    try:
+        n1 = nd['node_' + str(elem.nodes[0].int_label)].blender_object
+    except KeyError:
+        return {'NODE1_NOTFOUND'}
+    
+    try:
+        n2 = nd['node_' + str(elem.nodes[1].int_label)].blender_object
+    except KeyError:
+        return {'NODE2_NOTFOUND'}
+
+    # nodes' objects
+    n1OBJ = bpy.data.objects[n1]
+    n2OBJ = bpy.data.objects[n2]
+
+    try:
+
+        set_active_collection('joints')
+        elcol = bpy.data.collections.new(name = elem.name)
+        bpy.data.collections['joints'].children.link(elcol)
+        set_active_collection(elcol.name)
+        
+        # load the wireframe total joint object from the library
+        lib_path = os.path.join(mbs.addon_path,\
+            'library', 'joints.blend', 'Object')
+        bpy.ops.wm.append(directory = lib_path, filename = 'deformable displacement')
+        
+        ddOBJ = bpy.context.selected_objects[0]
+        ddOBJ.name = elem.name
+        RF1 = bpy.context.selected_objects[1]
+        RF1.name = elem.name + '_RF1'
+        RF2 = ddOBJ = bpy.context.selected_objects[2]
+        RF2.name = elem.name + '_RF2'
+
+        # place the joint objects in the position defined relative to node 1
+        ddOBJ.location = elem.offsets[0].value
+        ddOBJ.rotation_mode = 'QUATERNION'
+        ddOBJ.rotation_quaternion = Quaternion(elem.rotoffsets[0].value[0:])
+
+        RF1.location = elem.offsets[0].value
+        RF1.rotation_mode = 'QUATERNION'
+        RF1.rotation_quaternion = Quaternion(elem.rotoffsets[0].value[0:])
+
+        RF2.location = elem.offsets[1].value
+        RF2.rotation_mode = 'QUATERNION'
+        RF2.rotation_quaternion = Quaternion(elem.rotoffsets[1].value[0:])
+
+        # automatic scaling
+        s = (.5/sqrt(3.))*(n1OBJ.scale.magnitude + n2OBJ.scale.magnitude)
+        ddOBJ.scale = Vector((s, s, s))
+
+        # parenting of wireframe objects
+        parenting(ddOBJ, n1OBJ)
+        parenting(RF1, n1OBJ)
+        parenting(RF2, n2OBJ)
+
+        # link objects to element collection
+        elcol.objects.link(n1OBJ)
+        elcol.objects.link(n2OBJ)
+        set_active_collection('Master Collection')
+
+        return {'FINISHED'}
+    except FileNotFoundError:
+        return {'LIBRARY_ERROR'}
+    except KeyError:
+        return {'COLLECTION_ERROR'}
+# -----------------------------------------------------------
+# end of spawn_deformable_displacament_joint_element(elem, context) function
+
+## Imports a Deformable Displacement Joint in the scene
 class BLENDYN_OT_import_deformable_displacement(bpy.types.Operator):
     bl_idname = "blendyn.import_deformable_displacement"
     bl_label = "MBDyn deformable displacement joint element importer"
@@ -120,7 +192,29 @@ class BLENDYN_OT_import_deformable_displacement(bpy.types.Operator):
     
         try:
             elem = ed['deformable_displacement_' + str(self.int_label)]
-            return spawn_defdispj_element(elem, context)
+            retval =  spawn_deformable_displacement_joint_element(elem, context)
+            if retval == {'OBJECT_EXISTS'}:
+                eldbmsg(retval, type(self).__name__ + '::execute()', elem)
+                return {'CANCELLED'}
+            elif retval == {'NODE1_NOTFOUND'}:
+                eldbmsg(retval, type(self).__name__ + '::execute()', elem)
+                return {'CANCELLED'}
+            elif retval == {'NODE2_NOTFOUND'}:
+                eldbmsg(retval, type(self).__name__ + '::execute()', elem)
+                return {'CANCELLED'}
+            elif retval == {'COLLECTION_ERROR'}:
+                eldbmsf(retval, type(self).__name__ + '::execute()', elem)
+                return {'CANCELLED'}
+            elif retval == {'LIBRARY_ERROR'}:
+                eldbmsg(retval, type(self).__name__ + '::execute()', elem)
+                return {'CANCELLED'}
+            elif retval == {'FINISHED'}:
+                eldbmsg({'IMPORT_SUCCESS'}, type(self).__name__ + '::execute()', elem)
+                return retval
+            else:
+                # Should not be reached
+                return retval
+
         except KeyError:
             eldbmsg({'DICT_ERROR'}, type(self).__name__ + '::execute()', elem)
             return {'CANCELLED'}
