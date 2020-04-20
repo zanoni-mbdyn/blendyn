a40a8b432049aea331ca167c7b3648cc08d8519b
Andrea Zanoni <andrea.zanoni@polimi.it>
2020-04-20 14:49:26 +0200

    Cleanup

--- componentlib.py
+++ componentlib.py
@@ -83,8 +83,8 @@ def update_elem_str_idx(self, context):
 def add_mesh_component(context, component):
     """ Finalized a component definition by creating the armature 
         controlling the component deformation, based on the component
-        elements definitions, and assigning it to the selected mesh
-        object """
+        elements definitions, and (optionally) assigning it to the 
+        selected mesh object """
 
     mbs = context.scene.mbdyn
     nd = mbs.nodes
@@ -92,21 +92,27 @@ def add_mesh_component(context, component):
 
     # helper functions: add bones to armature of component for each
     # type of elemens
+
+#     def add_comp_armature_bones_plate(armOBJ, armature, component, celem):
+#         # shell and membrane elements
+#         elem = ed[celem.elem]
+#         elcol = bpy.data.collections[elem.name]
+#         node_objs = []
+# 
+#         # find element nodes' objects
+#         for obj in elcol.objects:
+#             try:
+#                 node_objs.append(nd[obj.mbdyn.dkey])
+#             except KeyError:
+#                 pass
+# 
+#         N1 = elcol.objects[
+
     def add_comp_armature_bones_beam3(armOBJ, armature, component, celem):
         elem = ed[celem.elem]
         elcol = bpy.data.collections[elem.name]
         node_objs = []
-        # find element nodes' objects
-        for obj in elcol.objects:
-            try:
-                node_objs.append(nd[obj.mbdyn.dkey])
-            except KeyError:
-                pass
-        for elnode in elem.nodes:
-            for node in nd:
-                if node.int_label == elnode.int_label:
-                    node_objs.append(bpy.data.objects[node.blender_object])
-                    break
+
         RF1 = elcol.objects[elem.name + '_RF1'] 
         RF2 = elcol.objects[elem.name + '_RF2']
         RF3 = elcol.objects[elem.name + '_RF3']
@@ -131,17 +137,17 @@ def add_mesh_component(context, component):
         edit_bones = armature.edit_bones 
 
         # node 1
-        bN1 = edit_bones.new(elem.name + '_' + node_objs[0].name)
+        bN1 = edit_bones.new(RF1.name)
         bN1.head = pRF1
         bN1.tail = pRF1 + 0.001*t1*L1
         bN1.use_deform = False
         # node 2
-        bN2 = edit_bones.new(elem.name + '_' + node_objs[1].name)
+        bN2 = edit_bones.new(RF2.name)
         bN2.head = pRF2 - 0.001*L1*t2
         bN2.tail = pRF2 + 0.001*L2*t2
         bN2.use_deform = False
         # node 3
-        bN3 = edit_bones.new(elem.name + '_' + node_objs[2].name)
+        bN3 = edit_bones.new(RF3.name)
         bN3.head = pRF3 - 0.001*L1*t3
         bN3.tail = pRF3
         bN3.use_deform = False
@@ -167,24 +173,24 @@ def add_mesh_component(context, component):
         V1 = armOBJ.pose.bones[elem.name + '_V1']
         stV1N2 = V1.constraints.new(type = 'STRETCH_TO')
         stV1N2.target = armOBJ
-        stV1N2.subtarget = elem.name + '_' + node_objs[1].name
+        stV1N2.subtarget = RF2.name
         V1 = armOBJ.data.bones[elem.name + '_V1']
         V1.bbone_handle_type_start = 'TANGENT'
-        V1.bbone_custom_handle_start = armOBJ.data.bones[elem.name + '_' + node_objs[0].name]
+        V1.bbone_custom_handle_start = armOBJ.data.bones[RF1.name]
         V1.bbone_handle_type_end = 'TANGENT'
-        V1.bbone_custom_handle_end = armOBJ.data.bones[elem.name + '_' + node_objs[1].name]
+        V1.bbone_custom_handle_end = armOBJ.data.bones[RF2.name]
         # volume 2
         V2 = armOBJ.pose.bones[elem.name + '_V2']
         stV2N3 = V2.constraints.new(type = 'STRETCH_TO')
         stV2N3.target = armOBJ
-        stV2N3.subtarget = elem.name + '_' + node_objs[2].name
+        stV2N3.subtarget = RF3.name
         V2 = armOBJ.data.bones[elem.name + '_V2']
         V2.bbone_handle_type_start = 'TANGENT'
-        V2.bbone_custom_handle_start = armOBJ.data.bones[elem.name + '_' + node_objs[1].name]
+        V2.bbone_custom_handle_start = armOBJ.data.bones[RF2.name]
         V2.bbone_handle_type_end = 'TANGENT'
-        V2.bbone_custom_handle_end = armOBJ.data.bones[elem.name + '_' + node_objs[2].name]
+        V2.bbone_custom_handle_end = armOBJ.data.bones[RF3.name]
         # node 1
-        N1 = armOBJ.pose.bones[elem.name + '_' + node_objs[0].name]
+        N1 = armOBJ.pose.bones[RF1.name]
         clN1RF1 = N1.constraints.new(type = 'COPY_LOCATION')
         clN1RF1.target = RF1
         crN1RF1 = N1.constraints.new(type = 'CHILD_OF')
@@ -197,7 +203,7 @@ def add_mesh_component(context, component):
         crN1RF1.target = RF1
         crN1RF1.inverse_matrix = RF1.matrix_world.inverted()
         # node 2
-        N2 = armOBJ.pose.bones[elem.name + '_' + node_objs[1].name]
+        N2 = armOBJ.pose.bones[RF2.name]
         clN2RF2 = N2.constraints.new(type = 'COPY_LOCATION')
         clN2RF2.target = RF2
         crN2RF2 = N2.constraints.new(type = 'CHILD_OF')
@@ -210,7 +216,7 @@ def add_mesh_component(context, component):
         crN2RF2.target = RF2
         crN2RF2.inverse_matrix = RF2.matrix_world.inverted()
         # node 3
-        N3 = armOBJ.pose.bones[elem.name + '_' + node_objs[2].name]
+        N3 = armOBJ.pose.bones[RF3.name]
         clN3RF3 = N3.constraints.new(type = 'COPY_LOCATION')
         clN3RF3.target = RF3
         crN3RF3 = N3.constraints.new(type = 'CHILD_OF')
@@ -231,17 +237,7 @@ def add_mesh_component(context, component):
         elem = ed[celem.elem]
         elcol = bpy.data.collections[elem.name]
         node_objs = []
-        # find element nodes' objects
-        for obj in elcol.objects:
-            try:
-                node_objs.append(nd[obj.mbdyn.dkey])
-            except KeyError:
-                pass
-        for elnode in elem.nodes:
-            for node in nd:
-                if node.int_label == elnode.int_label:
-                    node_objs.append(bpy.data.objects[node.blender_object])
-                    break
+        
         RF1 = elcol.objects[elem.name + '_RF1'] 
         RF2 = elcol.objects[elem.name + '_RF2']
         pRF1 = RF1.matrix_world.to_translation()
@@ -259,12 +255,12 @@ def add_mesh_component(context, component):
         edit_bones = armature.edit_bones 
 
         # node 1
-        bN1 = edit_bones.new(elem.name + '_' + node_objs[0].name)
+        bN1 = edit_bones.new(RF1.name)
         bN1.head = pRF1 
         bN1.tail = pRF1 + 0.001*t1*L
         bN1.use_deform = False
         # node 2
-        bN2 = edit_bones.new(elem.name + '_' + node_objs[1].name)
+        bN2 = edit_bones.new(RF2.name)
         bN2.head = pRF2 - 0.001*t1*L
         bN2.tail = pRF2
         bN2.use_deform = False
@@ -284,14 +280,14 @@ def add_mesh_component(context, component):
         V1 = armOBJ.pose.bones[elem.name + '_V1']
         stV1N2 = V1.constraints.new(type = 'STRETCH_TO')
         stV1N2.target = armOBJ
-        stV1N2.subtarget = elem.name + '_' + node_objs[1].name
+        stV1N2.subtarget = RF2.name
         V1 = armOBJ.data.bones['_V1']
         V1.bbone_handle_type_start = 'TANGENT'
-        V1.bbone_custom_handle_start = armOBJ.data.bones[elem.name + '_' + node_objs[0].name]
+        V1.bbone_custom_handle_start = armOBJ.data.bones[RF1.name]
         V1.bbone_handle_type_end = 'TANGENT'
-        V1.bbone_custom_handle_end = armOBJ.data.bones[elem.name + '_' + node_objs[1].name]
+        V1.bbone_custom_handle_end = armOBJ.data.bones[RF2.name]
         # node 1
-        N1 = armOBJ.pose.bones[node_objs[0].name]
+        N1 = armOBJ.pose.bones[RF1.name]
         clN1RF1 = N1.constraints.new(type = 'COPY_LOCATION')
         clN1RF1.target = RF1
         crN1RF1 = N1.constraints.new(type = 'CHILD_OF')
@@ -304,7 +300,7 @@ def add_mesh_component(context, component):
         crN1RF1.target = RF1
         crN1RF1.inverse_matrix = RF1.matrix_world.inverted()
         # node 2
-        N2 = armOBJ.pose.bones[node_objs[1].name]
+        N2 = armOBJ.pose.bones[RF2.name]
         clN2RF2 = N2.constraints.new(type = 'COPY_LOCATION')
         clN2RF2.target = RF2
         crN2RF2 = N2.constraints.new(type = 'CHILD_OF')
