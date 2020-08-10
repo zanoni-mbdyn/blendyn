9285e4b3cf3ca76ebb99e331337b9999c4529382
Andrea Zanoni <andrea.zanoni@polimi.it>
2020-08-10 21:40:39 +0200

    Modules installed in .config/blender/v.vv/scripts/modules/

--- utilslib.py
+++ utilslib.py
@@ -35,6 +35,7 @@ from bpy.props import *
 from bpy_extras.io_utils import ImportHelper
 
 from . dependencies import *
+import os
 import subprocess
 import imp
 
@@ -63,18 +64,19 @@ def install_pip():
 # -----------------------------------------------------------
 # end of install_pip() function 
 
-def install_and_import_module(module_name, package_name = None, global_name = None):
+def install_module(module_name, package_name = None, global_name = None):
     """
-    Installs the package through pip and attempts to import the installed module.
+    Installs a package through pip.
     :param module_name: Module to import.
     :param package_name: (Optional) Name of the package that needs to be installed.
                          If None it is assumed to be equal to the module_name.
     :param global_name: (Optional) Name under which the module is imported, as in
                         e.g., "import numpy as np"
-    :raises: subprocess.CalledProcessError and ImportError
-    source: https://github.com/robertguetzkow/blender-python-examples
+    :raises: subprocess.CalledProcessError 
+    ispired by: https://github.com/robertguetzkow/blender-python-examples
     """
     import importlib
+    mbs = bpy.context.scene.mbdyn
 
     if package_name is None:
         package_name = module_name
@@ -82,11 +84,17 @@ def install_and_import_module(module_name, package_name = None, global_name = No
     if global_name is None:
         global_name = module_name
 
+    # Check if modules/ folder is present. If not, attempts to create it
+    # TODO: make it an option, alternatively install globally
+    scriptsdir,addondir = os.path.split(mbs.addon_path)
+    modulesdir = os.path.join(scriptsdir, 'modules')
+    if not(os.path.isdir(modulesdir)):
+        os.mkdir(modulesdir, 0o755)
     # Try to install the package. This may fail with subprocess.CalledProcessError
-    subprocess.run([bpy.app.binary_path_python, "-m", "pip", "install", "--user", package_name], check = True)
+    subprocess.run([bpy.app.binary_path_python, "-m", "pip", "install", "-t", modulesdir, package_name], check = True)
 
 # -----------------------------------------------------------
-# end of install_and_import_module() function 
+# end of install_module() function 
 
 
 class BLENDYN_OT_install_dependencies(bpy.types.Operator):
@@ -108,7 +116,7 @@ class BLENDYN_OT_install_dependencies(bpy.types.Operator):
             install_pip()
             # deps is a global defines in dependencies.py
             for dependency in deps[self.feature]:
-                install_and_import_module(module_name = dependency.module,
+                install_module(module_name = dependency.module,
                                           package_name = dependency.package,
                                           global_name = dependency.name)
                 dependency.installed(True)
