55497367e3a916528816ea12485b891f30c8f951
Andrea Zanoni <andrea.zanoni@polimi.it>
2021-01-03 17:27:34 +0100

    Updated offsets to cope with new parenting()

--- revjlib.py
+++ revjlib.py
@@ -269,28 +269,28 @@ def spawn_revolute_hinge_element(elem, context):
         revjOBJ.scale = Vector(( s, s, s ))
 
         # joint offsets with respect to nodes
-        f1 = elem.offsets[0].value
-        f2 = elem.offsets[1].value
-        q1 = elem.rotoffsets[0].value
-        q2 = elem.rotoffsets[1].value
+        f1 = Vector(( elem.offsets[0].value[0:] ))
+        f2 = Vector(( elem.offsets[1].value[0:] ))
+        q1 = Quaternion(( elem.rotoffsets[0].value[0:] ))
+        q2 = Quaternion(( elem.rotoffsets[1].value[0:] ))
 
         # project offsets in global frame
         R1 = n1OBJ.rotation_quaternion.to_matrix()
         R2 = n2OBJ.rotation_quaternion.to_matrix()
-        p1 = Vector(( f1[0], f1[1], f1[2] ))
-        p2 = Vector(( f2[0], f2[1], f2[2] ))
+        p1 = n1OBJ.location + R1@f1
+        p2 = n2OBJ.location + R2@f2
 
         # place the joint object in the position defined relative to node 1
         revjOBJ.location = p1
         revjOBJ.rotation_mode = 'QUATERNION'
-        revjOBJ.rotation_quaternion = Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+        revjOBJ.rotation_quaternion = q1@n1OBJ.rotation_quaternion 
 
         # create an object representing the second RF used by the joint
         # for model debugging
         bpy.ops.object.empty_add(type = 'ARROWS', location = p2)
         RF2 = bpy.context.selected_objects[0]
         RF2.rotation_mode = 'QUATERNION'
-        RF2.rotation_quaternion = Quaternion(( q2[0], q2[1], q2[2], q2[3] ))
+        RF2.rotation_quaternion = q2@n2OBJ.rotation_quaternion 
         RF2.scale = .33*revjOBJ.scale
         RF2.name = revjOBJ.name + '_RF2'
         parenting(RF2, revjOBJ)
@@ -399,17 +399,17 @@ def spawn_revolute_pin_element(elem, context):
         revjOBJ.scale = Vector(( s, s, s ))
 
         # joint offsets with respect to nodes
-        f1 = elem.offsets[0].value
-        q1 = elem.rotoffsets[0].value
+        f1 = Vector(( elem.offsets[0].value[0:] ))
+        q1 = Quaternion(( elem.rotoffsets[0].value[0:] ))
 
         # project offsets in global frame
         R1 = n1OBJ.rotation_quaternion.to_matrix()
-        p1 = Vector(( f1[0], f1[1], f1[2] ))
+        p1 = n1OBJ.location + R1@f1
 
         # the position defined relative to node 1
         revjOBJ.location = p1
         revjOBJ.rotation_mode = 'QUATERNION'
-        revjOBJ.rotation_quaternion = Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+        revjOBJ.rotation_quaternion = q1@n1OBJ.rotation_quaternion 
 
         # set parenting of wireframe obj
         parenting(revjOBJ, n1OBJ)
@@ -520,28 +520,28 @@ def spawn_revolute_rot_element(elem, context):
         revjOBJ.scale = Vector(( s, s, s ))
 
         # joint offsets with respect to nodes
-        f1 = elem.offsets[0].value
-        f2 = elem.offsets[1].value
-        q1 = elem.rotoffsets[0].value
-        q2 = elem.rotoffsets[1].value
+        f1 = Vector(( elem.offsets[0].value[0:] ))
+        f2 = Vector(( elem.offsets[1].value[0:] ))
+        q1 = Quaternion(( elem.rotoffsets[0].value[0:] ))
+        q2 = Quaternion(( elem.rotoffsets[1].value[0:] ))
 
         # project offsets in global frame
         R1 = n1OBJ.rotation_quaternion.to_matrix()
         R2 = n2OBJ.rotation_quaternion.to_matrix()
-        p1 = Vector(( f1[0], f1[1], f1[2] ))
-        p2 = Vector(( f2[0], f2[1], f2[2] ))
+        p1 = n1OBJ.location + R1@f1
+        p2 = n2OBJ.location + R2@f2
 
         # place the joint object in the position defined relative to node 1
         revjOBJ.location = p1
         revjOBJ.rotation_mode = 'QUATERNION'
-        revjOBJ.rotation_quaternion = Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+        revjOBJ.rotation_quaternion = q1@n1OBJ.rotation_quaternion 
 
         # create an object representing the second RF used by the joint
         # for model debugging
         bpy.ops.object.empty_add(type = 'ARROWS', location = p2)
         RF2 = bpy.context.selected_objects[0]
         RF2.rotation_mode = 'QUATERNION'
-        RF2.rotation_quaternion = Quaternion(( q2[0], q2[1], q2[2], q2[3] ))
+        RF2.rotation_quaternion = q2@n2OBJ.rotation_quaternion 
         RF2.scale = .33*revjOBJ.scale
         RF2.name = revjOBJ.name + '_RF2'
         parenting(RF2, revjOBJ)
