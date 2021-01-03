cb02d1fba1cf130484886f3add70a72a621e164f
Andrea Zanoni <andrea.zanoni@polimi.it>
2021-01-03 17:42:35 +0100

    Updated offsets to cope with new parenting()

--- sphjlib.py
+++ sphjlib.py
@@ -198,28 +198,28 @@ def spawn_spherical_hinge_element(elem, context):
         sphjOBJ.scale = Vector(( s, s, s ))
 
         # joint offsets with respect to nodes
-        f1 = elem.offsets[0].value
-        f2 = elem.offsets[1].value
-        q1 = elem.rotoffsets[0].value
-        q2 = elem.rotoffsets[1].value
+        f1 = Vector(( elem.offsets[0].value[0:] ))
+        f2 = Vector(( elem.offsets[1].value[0:] ))
+        q1 = Quaternion(( elem.rotoffsets[0].value[0:] ))
+        q2 = Quaternion(( elem.rotoffsets[1].value[0:] ))
     
         # project offsets in global frame
         R1 = n1OBJ.rotation_quaternion.to_matrix()
         R2 = n2OBJ.rotation_quaternion.to_matrix()
-        p1 = Vector(( f1[0], f1[1], f1[2] ))
-        p2 = Vector(( f2[0], f2[1], f2[2] ))
+        p1 = n1OBJ.location + R1@f1
+        p2 = n2OBJ.location + R2@f2
     
         # place the joint object in the position defined relative to node 1
         sphjOBJ.location = p1
         sphjOBJ.rotation_mode = 'QUATERNION'
-        sphjOBJ.rotation_quaternion = Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+        sphjOBJ.rotation_quaternion = q1@n1OBJ.rotation_quaternion 
 
         # create an object representing the second RF used by the joint
         # for model debugging
         bpy.ops.object.empty_add(type = 'ARROWS', location = p2)
         RF2 = bpy.context.selected_objects[0]
         RF2.rotation_mode = 'QUATERNION'
-        RF2.rotation_quaternion = Quaternion(( q2[0], q2[1], q2[2], q2[3] ))
+        RF2.rotation_quaternion = q2@n2OBJ.rotation_quaternion 
         RF2.scale = .33*sphjOBJ.scale
         RF2.name = sphjOBJ.name + '_RF2'
         parenting(RF2, sphjOBJ)
@@ -284,17 +284,17 @@ def spawn_spherical_pin_element(elem, context):
         sphjOBJ.scale = Vector(( s, s, s ))
 
         # joint offsets with respect to nodes
-        f1 = elem.offsets[0].value
-        q1 = elem.rotoffsets[0].value
+        f1 = Vector(( elem.offsets[0].value[0:] ))
+        q1 = Quaternion(( elem.rotoffsets[0].value[0:] ))
     
         # project offsets in global frame
         R1 = n1OBJ.rotation_quaternion.to_matrix()
-        p1 = Vector(( f1[0], f1[1], f1[2] ))
+        p1 = n1OBJ.location + R1@f1
     
         # place the joint object in the position defined relative to node 1
         sphjOBJ.location = p1
         sphjOBJ.rotation_mode = 'QUATERNION'
-        sphjOBJ.rotation_quaternion = Quaternion(( q1[0], q1[1], q1[2], q1[3] ))
+        sphjOBJ.rotation_quaternion = q1@n1OBJ.rotation_quaternion 
 
         # set parenting of wireframe obj
         parenting(sphjOBJ, n1OBJ)
